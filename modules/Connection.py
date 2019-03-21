fcd6da1f51386d4ed0b18d1a6fa9cd4b51adc3bc
Joe <jal126463@gmail.com>
2019-03-21 15:45:26 -0400

    Work on connection logic

--- modules/Connection.py
+++ modules/Connection.py
@@ -1,5 +1,6 @@
-from urllib import request
-from ssl import SSLContext, PROTOCOL_SSLv23
+from urllib import request as url
+from modules import Cookie
+import ssl, json
 
 """Connection module for Joe's Grade Checker
 
@@ -12,6 +13,8 @@ Load cookie from stored file and check if still valid
 if not, procede to login with new cookie
 get AssignmentSchedule.asp and pass html to grade parser
 """
+
+sslContext = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
 
 def readGrades(config):
     #Store config globally
@@ -30,13 +33,41 @@ def readGrades(config):
     
     #If we don't have a valid cookie, get a new one by logging in
     if (not valid):
-        cookie = getNewCookie(config)
-
+        cookie = getNewCookie()
 
 def cookieIsValid(cookie):
-    #TODO: write function
-    pass
+    """Check validity of cookie by performing a get request on the assignmentschedule url"""
 
-def getNewCookie(config):
-    #TODO: write function
-    pass 
+    host = cfg["Connection"]["Host"] + cfg ["Connection"]["Assignment"]
+    
+    print(host)
+
+def getNewCookie():
+    """Obtains a new, authenticated sessionid (cookie) to use when retreiving grades and such
+    
+    Should go as follows:
+    Make a simple get request to the root and store the given sessionid
+    Make a post request to the login url with the user's username and password
+    Then check if the given cookie is valid or not
+    If it is, contine execution
+    If its not, throw an exception and stop program
+    """
+    
+    #First, get sessionid by making a GET request
+    host = cfg["Connection"]["Host"]
+    request = url.urlopen(host, context = sslContext)
+    sessionid = request.getheader("Set-Cookie").split(";")[0]
+
+    #Next, perform a login request via POST
+    data = getData()
+
+    print(data)
+
+def getData():
+    """Constructs a special data string for submition to server"""
+    base = Cookie.base
+    base = json.loads(base)
+    base["acctParms"]["acctName"] = cfg["Login"]["Username"]
+    base["acctParms"]["valuePwdOld"] = cfg["Login"]["Password"]
+
+    print(json.dumps(base, indent=4))
