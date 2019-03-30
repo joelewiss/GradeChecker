01c8e0e6520a23c43b74c8c48c96366dd1e52110
Joe <jal126463@gmail.com>
2019-03-29 23:13:07 -0400

    Changed output module formatting

--- modules/output/console.py
+++ modules/output/console.py
@@ -1,3 +1,4 @@
 def output(grades):
+    print("Course\tTitle\tGrade\tScore\tTeacher")
     for grade in grades:
         print("{0}\t{1}\t{2}\t{3}\t{4}".format(grade.course, grade.title, grade.letter, grade.score, grade.teacher))
