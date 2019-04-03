fe723b744b2d18c286a226037a8977e76e0ce578
Joe <jal126463@gmail.com>
2019-04-02 22:00:58 -0400

    Work on graphite module

---
+++ modules/output/graphite.py
@@ -0,0 +1,20 @@
+import sys, time
+from modules import Config
+
+def output(grades):
+    config = Config.getConfig()
+    prefix = config["Graphite"]["Prefix"]
+    metrictime = time.time()
+    output = ""
+
+    #Good lord what is this
+    for grade in grades:
+        output += prefix + "."
+        output += grade.title.split("(")[0].strip().lower().replace(" ", "")
+        output += " " + str(grade.score) + " "
+        output += str(metrictime)
+        output += "\n"
+
+    print(output)
+
+    #ToDo: write tcp connection logic
