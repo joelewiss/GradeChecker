2c59f50164a75272c556249e75af3d2447ea5fc2
Joe <jal126463@gmail.com>
2019-04-04 16:02:27 -0400

    Finished graphite output module and added config options

--- modules/output/graphite.py
+++ modules/output/graphite.py
@@ -1,4 +1,5 @@
 import sys, time
+import telnetlib as tn
 from modules import Config
 
 def output(grades):
@@ -7,15 +8,34 @@ def output(grades):
     metrictime = time.time()
     output = ""
 
-    #Good lord what is this
+    #Formats grades for transmittion to graphite
     for grade in grades:
         output += prefix + "."
+        
+        #Only includes the first two words (split on spaces) in the metric name
         title = grade.title.split(" ")
         output += title[0].strip().lower() + title[1].strip().lower()
+        
         output += " " + str(grade.score) + " "
-        output += str(metrictime)
+        output += str(int(metrictime))
         output += "\n"
 
     print(output)
 
-    #ToDo: write tcp connection logic
+    host = config["Graphite"]["Host"].split(":")
+    port = host[1]
+    host = host[0]
+   
+    try: 
+        connection = tn.Telnet(host, port)
+        print("established connection")
+    except ConnectionRefusedError:
+        #I really need a console printing system 
+        print("Error: Could not establish connection")
+        return
+
+    print("writing data...")
+    connection.write(output.encode("ascii"))
+    connection.close()
+
+    print("\ndone!")
