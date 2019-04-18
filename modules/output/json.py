b9ad62ac4f9f2dabcf1f7a177bbb5b4621bfb527
Joe <jal126463@gmail.com>
2019-04-17 22:20:50 -0400

    Updated json output module

--- modules/output/json.py
+++ modules/output/json.py
@@ -4,6 +4,7 @@ class GradeEncoder(json.JSONEncoder):
     def default(self, obj):
         return {"Course": obj.course,
                 "Title": obj.title,
+                "Period": obj.period,
                 "Letter": obj.letter,
                 "Score": obj.score,
                 "Teacher": obj.teacher
